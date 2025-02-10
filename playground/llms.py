5b23742cfaab11817dba2f99ef42d8f87579f807
Micheal Lanham <cxbxmxcx@gmail.com>
2025-02-09 17:11:09 -0700

    Resolved issue with updated client support

--- playground/llms.py
+++ playground/llms.py
@@ -7,20 +7,28 @@ load_dotenv()
 def get_llm_client():
     api_type = os.getenv("API_TYPE")
 
-    if api_type == "openai":
+    if api_type:
+        if api_type == "openai":
+            from openai import OpenAI
+
+            client = OpenAI(
+                api_key=os.getenv("OPENAI_API_KEY"),
+                api_version=os.getenv("OPENAI_API_VERSION"),
+            )
+        elif api_type == "azure":
+            from openai import AzureOpenAI
+
+            client = AzureOpenAI(
+                azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+                api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+                api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+            )
+    else:
+        # no key revert back to openai
         from openai import OpenAI
 
         client = OpenAI(
-            api_key=os.getenv("OPENAI_API_KEY"),
-            api_version=os.getenv("OPENAI_API_VERSION"),
-        )
-    elif api_type == "azure":
-        from openai import AzureOpenAI
-
-        client = AzureOpenAI(
-            azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-            api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-            api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+            api_key=os.getenv("OPENAI_API_KEY"),            
         )
 
     return client
